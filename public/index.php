9814baa9f3d12b08e3944f68dbc42a91befd588b
holic <holic@dealertire.kr>
2021-08-13 09:06:02 +0900

    api helloworld test

--- public/index.php
+++ public/index.php
@@ -25,24 +25,30 @@ $app->get('/{name}', function (Request $request, Response $response, array $args
     return $response;
 });
 
-$_url = parse_url($_SERVER['REQUEST_URI']);
-$_routes = explode('/', $_url['path']);
-$_baseRoute = $_routes[1];
-
-switch ($_baseRoute) {
-    case 'order':
-        $_routeFile = __DIR__ . '/../routes/order.php';
-        break;
-    default:
-        $_routeFile = __DIR__ . '/../routes/tire.php';
-        break;
-}
-
-if (file_exists($_routeFile)) {
-    require $_routeFile;
-} else {
-    die('Invalid API request');
-}
+//$app->get('/openapi', function ($request, $response, $args) {
+//    $swagger = scan(__DIR__ . '/document/document.yaml');
+//    $response->getBody()->write(json_encode($swagger));
+//    return $response->withHeader('Content-Type', 'application/json');
+//});
+//
+//$_url = parse_url($_SERVER['REQUEST_URI']);
+//$_routes = explode('/', $_url['path']);
+//$_baseRoute = $_routes[1];
+//
+//switch ($_baseRoute) {
+//    case 'order':
+//        $_routeFile = __DIR__ . '/../routes/order.php';
+//        break;
+//    default:
+//        $_routeFile = __DIR__ . '/../routes/tire.php';
+//        break;
+//}
+//
+//if (file_exists($_routeFile)) {
+//    require $_routeFile;
+//} else {
+//    die('Invalid API request');
+//}
 
 $app->run();
 ?>
