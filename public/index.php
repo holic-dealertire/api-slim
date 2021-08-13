ae4f1c628bf879ab2efac0c1d308453507c2159b
holic <holic@dealertire.kr>
2021-08-13 09:35:05 +0900

    api helloworld test

--- public/index.php
+++ public/index.php
@@ -9,8 +9,8 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../config/db.php';
+require '../vendor/autoload.php';
+require '../config/db.php';
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
@@ -25,30 +25,30 @@ $app->get('/{name}', function (Request $request, Response $response, array $args
     return $response;
 });
 
-//$app->get('/openapi', function ($request, $response, $args) {
-//    $swagger = scan(__DIR__ . '/document/document.yaml');
-//    $response->getBody()->write(json_encode($swagger));
-//    return $response->withHeader('Content-Type', 'application/json');
-//});
-//
-//$_url = parse_url($_SERVER['REQUEST_URI']);
-//$_routes = explode('/', $_url['path']);
-//$_baseRoute = $_routes[1];
-//
-//switch ($_baseRoute) {
-//    case 'order':
-//        $_routeFile = __DIR__ . '/../routes/order.php';
-//        break;
-//    default:
-//        $_routeFile = __DIR__ . '/../routes/tire.php';
-//        break;
-//}
-//
-//if (file_exists($_routeFile)) {
-//    require $_routeFile;
-//} else {
-//    die('Invalid API request');
-//}
+$app->get('/openapi', function ($request, $response, $args) {
+    $swagger = scan('../document/document.yaml');
+    $response->getBody()->write(json_encode($swagger));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$_url = parse_url($_SERVER['REQUEST_URI']);
+$_routes = explode('/', $_url['path']);
+$_baseRoute = $_routes[1];
+
+switch ($_baseRoute) {
+    case 'order':
+        $_routeFile = '../routes/order.php';
+        break;
+    default:
+        $_routeFile = '../routes/tire.php';
+        break;
+}
+
+if (file_exists($_routeFile)) {
+    require $_routeFile;
+} else {
+    die('Invalid API request');
+}
 
 $app->run();
 ?>
