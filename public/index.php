47b399a985de5040ce498792a3847ee2d0c746ea
holic <holic@dealertire.kr>
2021-08-16 06:18:48 +0900

    api helloworld test

--- public/index.php
+++ public/index.php
@@ -9,8 +9,8 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require dirname(__FILE__) . '/../vendor/autoload.php';
-require dirname(__FILE__) . '/../config/db.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config/db.php';
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
